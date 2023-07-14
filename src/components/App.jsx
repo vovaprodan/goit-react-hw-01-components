beb2fd6d542dfe5172904a6c829164190dc407a7
vovaprodan <121820923+vovaprodan@users.noreply.github.com>
2023-07-14 19:50:36 +0200

    reat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
 import user from '../user.json';
+import friends from '../friends.json';
 import data from '../data.json'
+import transactions from '../transactions.json'
 import { Profile } from './Profile/Profile'
 import { Statistics } from './Statistics/Statistics'
+import { FriendList } from './FriendList/FriendList'
+import {TransactionHistory} from './TransactionHistory/TransactionHistory'
 export const App = () => {
   return (<>
   <Profile
@@ -12,7 +16,9 @@ export const App = () => {
   stats={user.stats}
     />
   <Statistics 
-  title="Upload stats" stats={data} />
+      title="Upload stats" stats={data} />
+    <FriendList friends={friends} />;
+    <TransactionHistory items={transactions} />;
   </>
    )
 };
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import css from './FriendListItem.module.css'
+
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+    return <li className={css.item}>
+    <span className={css.status}>{isOnline}</span>
+    <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+    <p className={css.name}>{name}</p>
+</li>
+}
+
+FriendListItem.prototype = {
+    avatar: PropTypes.string,
+    name: PropTypes.string,
+    isOnline: PropTypes.bool 
+}
