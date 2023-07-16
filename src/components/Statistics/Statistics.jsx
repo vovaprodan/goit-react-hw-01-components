8d312af206577c39464acf54e8f0eb9c3e755413
vovaprodan <121820923+vovaprodan@users.noreply.github.com>
2023-07-16 21:14:29 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import css from './Statistics.module.css'
 export const Statistics = ({ title, stats }) => {
     return <section className={css.statistics}>
-        <h2 className={css.title}>{title}</h2>
+       {title && <h2 className={css.title}>{title}</h2>}
         <ul className={css.stat_list}>
     
         {stats.map(stat => (  
